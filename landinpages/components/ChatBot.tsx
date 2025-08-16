1a37e23ea54f3a41e1c892779bbfe2b8c55c693b
Roneru-k <20231035@itla.edu.do>
2025-08-16 10:52:43 -0400

    Update ChatBot.tsx

--- landinpages/components/ChatBot.tsx
+++ landinpages/components/ChatBot.tsx
@@ -4,36 +4,36 @@ import { createChat } from '@n8n/chat';
 
 export const ChatBot = () => {
 	useEffect(() => {
-		createChat({
-			webhookUrl: 'http://localhost:5678/webhook/f40137f1-94db-4528-ba15-b8ab0b1c3774/chat',
-			webhookConfig: {
-				method: 'POST',
-				headers: {}
-			},
-			target: '#n8n-chat',
-			mode: 'window',
-			chatInputKey: 'chatInput',
-			chatSessionKey: 'sessionId',
-			loadPreviousSession: true,
-			metadata: {},
-			showWelcomeScreen: false,
-			defaultLanguage: 'en',
-			initialMessages: [
-				'¡Hola! 👋',
-				'Soy **ZypherBot**, tu asistente virtual. ¿En qué puedo ayudarte con Zypher hoy?'
-			],
-			i18n: {
-				es: {
-					title: '¡Bienvenido a ZypherBot! 🤖',
-					subtitle: 'Chatea con nosotros. Estamos aquí para ayudarte 24/7.',
-					footer: '',
-					getStarted: 'Nueva conversación',
-					inputPlaceholder: 'Escribe tu pregunta aquí...',
-					closeButtonTooltip: 'Cerrar chat'
-				}
-			},
-		});
+createChat({
+	webhookUrl: 'http://localhost:5678/webhook/f40137f1-94db-4528-ba15-b8ab0b1c3774/chat',
+	webhookConfig: {
+		method: 'POST',
+		headers: {}
+	},
+	target: '#n8n-chat',
+	mode: 'window',
+	chatInputKey: 'chatInput',
+	chatSessionKey: 'sessionId',
+	loadPreviousSession: true,
+	metadata: {},
+	showWelcomeScreen: false,
+	defaultLanguage: 'en',
+	initialMessages: [
+		'Hola que tal! 👋',
+		'Mi nombre es Zypherbot. ¿En qué puedo ayudarte hoy?'
+	],
+	i18n: {
+		en: {
+            title: 'Hola 👋',
+            subtitle: "Bienvenido a Zypher.",
+            footer: '',
+            getStarted: 'Nueva Conversacion',
+            inputPlaceholder: 'Nueva conversación',
+            closeButtonTooltip: ''
+        },
+	},
+});
 	}, []);
 
-	return <div id="n8n-chat"></div>;
+	return (<div></div>);
 };
